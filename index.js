d45b0cba33a74460d9a307d264299f8b000aaa7a
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2024-03-31 11:42:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import ffmpeg from 'ffmpeg';
 import cors from 'cors';
 import { getLyrics, getSong } from 'genius-lyrics-api';
+import zlib from 'zlib'
 const cachedResults = {};
 import path from 'path';
 import fs from 'fs';
@@ -213,23 +214,39 @@ app.get('/serveImage', async (req, res) => {
 
     }
 });
-app.get('/stream', async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*') 
+app.get('/stream',   async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin' , '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Headers' , 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     try {
-        const videoUrl = req.query.url;
-        const videoInfo = await ytdl.getInfo(videoUrl);
-        // only audio
-        const format = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestaudio' });
-        const audio =   ytdl.filterFormats(videoInfo.formats, 'videoandaudio').find((format) => format.container === 'mp4');
+        const videoUrl = req.query.url; 
+        const audio = ytdl(videoUrl, {filter:'audio', quality:'highestaudio'}); 
+        const info = await ytdl.getInfo(videoUrl);
         res.setHeader('Content-Type', 'audio/mpeg');
-        res.setHeader('Content-Disposition', `attachment; filename="${videoInfo.videoDetails.title}.mp3"`); 
+        res.setHeader('Content-Disposition', `attachment; filename="${info.videoDetails.title}.mp3"`);
         res.setHeader('Accept-Ranges', 'bytes');
         res.setHeader('Connection', 'keep-alive');
-        res.redirect(audio.url); 
-        } catch (error) {  
-        res.status(400).send('Invalid URL');
+        res.setHeader('Cache-Control', 'no-cache'); 
+        let virtualFile =  []
+        audio.on('data', (chunk) => {
+            virtualFile.push(chunk);
+        });
+        audio.on('end', () => {  
+            let compressed = zlib.gzipSync(Buffer.concat(virtualFile)); 
+            res.setHeader('Content-Encoding', 'gzip');
+            res.setHeader('Content-Length', compressed.length);
+            res.setHeader('Cache-Control', 'public, max-age=31536000');
+            res.setHeader('Expires', new Date(Date.now() + 31536000000).toUTCString());
+            res.send(compressed);
+        })
+        audio.on('error', (error) => {
+            console.log(error)
+            res.send(error);
+        });
+        } catch (error) {   
+            console.log(error)
+            res.send(error)
         }
 
    
@@ -237,7 +254,7 @@ app.get('/stream', async (req, res) => {
 
 app.use(express.static('./')); 
 app.get('/', (req,res) => {
-    res.json({timestamp:Date.now(), location:'Washington DC Us East'})
+    res.json({timestamp:Date.now(), Location:'Washington US East'})
 })
  
  
@@ -262,57 +279,53 @@ app.get('/metadata', async (req, res) => {
 
 
  
-app.get('/search', async (req, res) => {
+app.get('/search',  async (req, res) => {
+  
     const { query } = req.query;
     if (!query) {
         return res.status(400).send('Query is required');
     } 
+   
     if (Object.keys(cachedResults).includes(query.toLowerCase())) {
         let matching = Object.keys(cachedResults).filter((key) => key.includes(query.toLowerCase()));
         let results = matching.map((key) => cachedResults[key]);
         return res.json([].concat(...results));
     }
-    const results = await yts(query);  
-    results.videos = results.videos.map((video) => { 
-        // remove video if too long
-        if(video.timestamp.includes('hour') || video.timestamp.includes('day') || video.timestamp.includes('week') || video.timestamp.includes('month') || video.timestamp.includes('year')){
-            return;
-        }
-        video.title = video.title.replace(/\[.*\]/, '').replace(/\(.*\)/, '');
-        // split ft. or ft or feat or featuring
-        if(video.title.includes('ft.')){
-            video.title = video.title.split('ft.')[0];
-        }
-        // splice long titles
-        if(video.title.length > 50){
-            video.title = video.title.slice(0, 50);
-        }
-    // exclude non-registered artists
-        let artist = video.author.name.toLowerCase();
-        let isRegistered  = registeredArtists.includes(artist);
-        // if name registered then dont return any other artist
-        if(!isRegistered && registeredArtists.includes(query)){
-            console.log('not registered', artist, query.toLowerCase());
-            return;
-        }
-        let data = {
-            url:  `${urls.main}/stream?url=${video.url}`,
-            title: video.title,
-            id: video.videoId, 
-            thumbnail: `${urls.main}/serveImage?url=${video.image}`,
-            description: video.description,
-            duration: video.duration.seconds,
-            views: video.views,
-            age: video.ago,
-            artist: video.author.name,
-            artistUrl: video.author.url
-        } 
-        return data;
-    })
-    // remove nulls
-    results.videos = results.videos.filter((video) => video !== undefined);
-    cachedResults[query.toLowerCase()] = results.videos;
-    res.json(results.videos);
+     try { 
+        let youtubeData = await yts(query);
+        let results = youtubeData.videos.map(async (video) => {    
+            
+            let videoadata = await ytdl.getInfo(video.url)
+            video.relatedVideos = videoadata.related_videos.map(async (video) => {  
+                video.url = `${urls.main}/stream?url=https://www.youtube.com/watch?v=${video.id}`;
+                video.thumbnail = `${urls.main}/serveImage?url=${video.thumbnails[0].url}`;
+                return video;
+            });
+            video.relatedVideos = await Promise.all(video.relatedVideos);
+            video.bitrate = (await videoadata).formats[0].audioBitrate;
+            if(video.title.includes('ft.')){
+                video.title = video.title.split('ft.')[0];
+            }
+            video.keywords = video.description.split('#').map((keyword) => keyword.split(' ')[0]);
+            // splice long titles
+            if(video.title.length > 50){
+                video.title = video.title.slice(0, 50);
+            }
+            return video;
+        })
+        // only allow https://musicly.rf.gd to access this endpoint 
+        
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        res.setHeader('Cache-Control', 'public, max-age=3600');
+        res.setHeader('Expires', new Date(Date.now() + 3600000).toUTCString());
+        cachedResults[query.toLowerCase()] = await Promise.all(results);
+        res.json(await Promise.all(results));
+    }
+        catch(errr){
+            console.log(errr)
+        }    
 })
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
